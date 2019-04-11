4e38634f68e5add7e89ea92b3ea7bde10a49cc91
Rob Brown <brown4homes@yahoo.com>
2019-04-11 11:57:07 -0700

    Added three products to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'Cream Cheese',
+              description: 'Delicious on your bagel.',
+              price: 3.35)
+
+Product.create(name: 'X-Ray Glass',
+              description: 'Great for a few laughs.',
+              price: 59.99)
+
+Product.create(name: 'Catnip',
+              description: 'Drives you feline crazy.',
+              price: 5.99)
