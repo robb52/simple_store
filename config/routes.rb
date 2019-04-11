66f59aaf0ecc5862602774c08f258f8a1ddb265f
Rob Brown <brown4homes@yahoo.com>
2019-04-11 13:44:01 -0700

    Replaced custom routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'products', to: 'products#index', as: 'products'
+  resources :products, only:[:index, :show]
 
-  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
+  root 'products#index'
+
+  # get 'products', to: 'products#index', as: 'products'
+
+  # get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
 
 end
